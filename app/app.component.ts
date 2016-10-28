e5b87ee6c9af8df61ddc93b078bd4710c47dd81e
KJ <kj@neoswell.com>
2016-10-28 15:18:57 +0900

    Two-way data binding

--- app/app.component.ts
+++ app/app.component.ts
@@ -31,6 +31,7 @@ import { User } from './shared/model/user';
         <div class="col-sm-8">
           <div class="jumbotron" *ngIf="activeUser">
             <h2>{{ activeUser.name }} <small>{{ activeUser.username }}</small></h2>
+            <input type="text" class="form-control" [(ngModel)]="activeUser.name" />
           </div>
 
           <div class="jumbotron" *ngIf="!activeUser">
