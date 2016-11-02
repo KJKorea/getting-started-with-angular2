d891325070bb2cbc8fd84bc7feb5248d066a2771
agassikim <agassikim@KJ-MacBook.local>
2016-11-03 00:18:33 +0900

    Working with forms

--- app/users/user-form.component.ts
+++ app/users/user-form.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from '../shared/models/user';
 
 @Component({
   selector: 'user-form',
@@ -7,21 +8,27 @@ import { Component } from '@angular/core';
       padding: 10px;
       background: #ECF0F1;
       border-radius: 3px;
+      margin-bottom: 30px;
     }
   `],
   template: `
-    <form >
+    <form #form="ngForm">
+
+      {{ form.valid }}
+
       <div class="form-group">
-        <input type="text" class="form-control" name="name" required />        
+        <input type="text" class="form-control" name="name" required [(ngModel)]="newUser.name" #name="ngModel" />        
       </div>
       <div class="form-group">
-        <input type="text" class="form-control" name="username" required />        
+        <input type="text" class="form-control" name="username" required [(ngModel)]="newUser.username" #username="ngModel" />        
       </div>
 
-      <button type="submit" class="btn btn-lg btn-block btn-primary">
+      <button type="submit" class="btn btn-lg btn-block btn-primary" [disabled]="form.invalid">
         Create a User
       </button>
     </form>
   `
 })
-export class UserFormComponent {}
+export class UserFormComponent {
+  newUser: User = new User();
+}
