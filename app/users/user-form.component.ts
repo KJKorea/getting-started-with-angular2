6fe201c86f3175bea9f92c7d28a81946871fb47c
KJ <kj@neoswell.com>
2016-11-09 16:14:50 +0900

    Submitting Forms

--- app/users/user-form.component.ts
+++ app/users/user-form.component.ts
@@ -12,15 +12,24 @@ import { User } from '../shared/models/user';
     }
   `],
   template: `
-    <form #form="ngForm">
+    <form #form="ngForm" (ngSubmit)="onSubmit()" *ngIf="active">
 
-      <div class="form-group" [ngClass]="{ 'has-error': name.invalid && name.touched }">
-        <input type="text" class="form-control" name="name" placeholder="Name" required [(ngModel)]="newUser.name" #name="ngModel" />       
-        <span class="help-block" *ngIf="name.invalid && name.touched">Name is required</span>
+      <div class="form-group" 
+           [ngClass]="{ 'has-error': name.invalid && name.touched }">
+        <input type="text" class="form-control" name="name" placeholder="Name" required 
+               [(ngModel)]="newUser.name" #name="ngModel" />       
+        <span class="help-block" 
+              *ngIf="name.invalid && name.touched">
+              Name is required</span>
       </div>
-      <div class="form-group" [ngClass]="{ 'has-error': username.invalid && username.touched }">
-        <input type="text" class="form-control" name="username" placeholder="Username" required [(ngModel)]="newUser.username" #username="ngModel" />
-        <span class="help-block" *ngIf="username.invalid && username.touched">Username is required</span>   
+
+      <div class="form-group"   
+           [ngClass]="{ 'has-error': username.invalid && username.touched }">
+        <input type="text" class="form-control" name="username" placeholder="Username" required 
+               [(ngModel)]="newUser.username" #username="ngModel" />
+        <span class="help-block" 
+              *ngIf="username.invalid && username.touched">
+              Username is required</span>   
       </div>
 
       <button type="submit" class="btn btn-lg btn-block btn-primary" [disabled]="form.invalid">
@@ -31,4 +40,13 @@ import { User } from '../shared/models/user';
 })
 export class UserFormComponent {
   newUser: User = new User();
+  active: boolean = true;
+
+  onSubmit() {
+    console.log(this.newUser);
+
+    this.newUser = new User();
+    this.active = false;
+    setTimeout(()=>this.active=true, 0);
+  }
 }
